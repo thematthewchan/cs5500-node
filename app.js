e9857084b05fd86cc592c64b35df2c8431f89280
thematthewchan <xtremematt911@gmail.com>
2022-10-28 19:57:35 -0400

    env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose'); // npm install mongoose
 const userController = require('./controllers/UserController');
 mongoose.connect('mongodb+srv://user:user@atlascluster.yhpg964.mongodb.net/?retryWrites=true&w=majority')
+app.get('/env', (req, res) => res.send(process.env));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 userController(app);
