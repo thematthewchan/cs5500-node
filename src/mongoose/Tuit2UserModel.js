235af005169c375dfc8c97c5030b4e095482673f
thematthewchan <xtremematt911@gmail.com>
2022-10-30 22:39:22 -0400

    make it better

--- src/mongoose/Tuit2UserModel.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file Implements mongoose model to CRUD
- * documents in the Tuit2User collection
- */
-import mongoose from "mongoose";
-import Tuit2UserSchema from "./Tuit2UserSchema";
-const Tuit2UserModel = mongoose.model('Tuit2UserModel', Tuit2UserSchema);
-export default Tuit2UserModel;
