509bd2ca1a4e770cd06a43e3cdec6d15b1e8c543
thematthewchan <xtremematt911@gmail.com>
2022-10-31 12:59:45 -0400

    make it better

--- app.mjs
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose'); // npm install mongoose
-const movieController = require('./src/controllers/UserController');
-mongoose.connect('mongodb+srv://user:user@atlascluster.yhpg964.mongodb.net/?retryWrites=true&w=majority')
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-movieController(app);
-module.exports = app;
